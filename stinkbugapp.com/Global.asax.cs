9b0161e637ab05603af19b5b8478d944283aa147
Erik Zettersten <erik@zettersten.com>
2013-04-27 01:12:18 -0700

    added some files, normalizing structure

--- stinkbugapp.com/Global.asax.cs
+++ stinkbugapp.com/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace stinkbugapp.com
@@ -16,7 +17,6 @@ namespace stinkbugapp.com
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
